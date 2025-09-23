4e86b5c6c2b87ded76db6add6ac9b54f92d73e3d
Devumi Hasangi <devumihasangi2002@gmail.com>
2025-09-23 21:10:00 +0530

    XSS & Information Exposure

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,10 +15,17 @@ import cartRouter from "./api/routes/cart.route";
 import addressRouter from "./api/routes/address.route";
 import loyaltyRoutes from "./api/routes/loyaltyRoutes";
 import referralRoutes from "./api/routes/referralRoutes";
+import helmet from "helmet";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+//Disable x-powered-by header to prevent information leakage
+app.disable("x-powered-by");
+
+//Use this to secure Express apps by setting various HTTP headers
+app.use(helmet());
 
 app.use(
   cors({
